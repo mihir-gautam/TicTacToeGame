aaeed21096a95acc3fd647610481804e3f161104
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-05 14:37:06 +0530

    [Mihir] Add Board Game created

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+            TicTacToe Game = new TicTacToe();
+            Game.TicTacToeGame();
+        }
+    }
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeProgram
+{
+    class TicTacToe
+    {
+        char[] board = new char[10];
+
+        public void TicTacToeGame()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+
+        }
+
+    }
+}
