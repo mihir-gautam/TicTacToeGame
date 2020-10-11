a604df36aaef99518e0a079e3ba66ddfae61f8e8
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-11 17:50:55 +0530

    [Mihir] Add functionality to play another game

--- Program.cs
+++ Program.cs
@@ -8,15 +8,22 @@ namespace TicTacToeProgram
         {
             Console.WriteLine("Welcome to TicTacToe Game");
             TicTacToeGame Game = new TicTacToeGame();
-            Game.TicTacToeBoard();
-            Game.ShowBoard();
-            char userChoice=Game.myChoice();
-            char compChoice;
-            if (userChoice == 'X')
-            compChoice = 'O';
-            else
-                compChoice = 'X';
-            Game.GamePlay(userChoice, compChoice);
+            int gameNumber = 1;
+            while (gameNumber == 1)
+            { 
+                Game.TicTacToeBoard();
+                Game.ShowBoard();
+                char userChoice = Game.myChoice();
+                char compChoice;
+                if (userChoice == 'X')
+                    compChoice = 'O';
+                else
+                    compChoice = 'X';
+                Game.ShowBoard();
+                Game.GamePlay(userChoice, compChoice);
+                Console.WriteLine("Do you want to play another game? \n1. Yes\n2. No");
+                gameNumber = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
